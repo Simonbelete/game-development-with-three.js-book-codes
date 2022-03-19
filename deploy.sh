15bac33a9b7e838fd191f33aa688f09e4788c78a
Simonbelete <simonbelete@gmail.com>
2022-03-19 17:26:59 +0300

    deploy configs

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git add -A
+git commit -m 'gh-page'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Simonbelete/game-development-with-three.js-book-codes.git main:gh-pages
+
+cd -
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/game-development-with-three.js-book-codes/'
+})
